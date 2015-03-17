c8e99709272b528622bf0217abb18c8772322857
Ziyad Parekh <ziyad@1stdibs.com>
2015-03-17 18:33:09 -0400

    Added some structs to mock githook payload

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,13 +1,39 @@
 package handlers
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
+
+	"github.com/ziyadparekh/runtime/logger"
 )
+
+type Event struct {
+	PullRequest PullRequest `json:"pull_request"`
+}
+
+type PullRequest struct {
+	Url       string `json:"url"`
+	State     string `json:"state"`
+	Title     string `json:"title"`
+	CreatedAt string `json:"created_at"`
+	User      User   `json:"user"`
+	Head      Commit `json:"head"`
+}
+
+type User struct {
+	Login  string `json:"login"`
+	Avatar string `json:"avatar_url"`
+	Url    string `json:"url"`
+}
+
+type Commit struct {
+	Label string `json:"label"`
+	Ref   string `json:"ref"`
+	Sha   string `json:"sha"`
+	URL   string `json:"url"`
+}
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
@@ -17,7 +43,17 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 func Payload(w http.ResponseWriter, r *http.Request) {
 	body, _ := ioutil.ReadAll(r.Body)
-	var out bytes.Buffer
-	json.Indent(&out, body, "", "\t")
-	out.WriteTo(os.Stdout)
+	defer r.Body.Close()
+	var event Event
+	if err := json.Unmarshal(body, &event); err != nil {
+		logger.Fatalf("%v", err)
+	}
+	out, err := json.MarshalIndent(event, "", "  ")
+	if err != nil {
+		logger.Fatalf("%v", err)
+	}
+	fmt.Println(string(out))
+	// var out bytes.Buffer
+	// json.Indent(&out, body, "", "\t")
+	// out.WriteTo(os.Stdout)
 }
